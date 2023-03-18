7c84693a084c41737e340b62dc97a3d3ce91e755
OrionAlzate <orion_alzate@hotmail.com>
2023-03-17 20:42:23 -0500

    react con componentes de navbar, banner y servicios

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,17 @@
+export function AcercaDe(){
+    return(
+        <>
+            <div class="container my-5">
+                <div class="row justify-content-center ">
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/tiendamoraazul-fba5e.appspot.com/o/producto1.png?alt=media&token=72c761e2-154c-4bf9-bf7d-a343a8a834bd" alt="producto1" class="img-fluid"></img>
+                    </div>
+                    <div class="col-12 col-md-4 text-center align-self-center">
+                        <h2>Tienda Mora Azul</h2>
+                        <p>(Acá va la descripcion del producto...)</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
